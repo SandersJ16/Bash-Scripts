43ea989b0872086df5e90c9a3875756c09934fce
Justin <justin@pointw.com>
2018-02-26 22:44:48 -0600

    Added support for case insensitive searches, $ regex and properly escaped double quotes

--- searchtext.bash
+++ searchtext.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+case_insenitive=false
 exclude_defaults=true
 search_term=""
 other_args=""
@@ -11,12 +12,16 @@ for input in "$@"; do
     search_term=`echo "$input" | sed 's/"/\\\"/'`
   elif [[ "$input" == "-v" ]]; then
     exclude_defaults=false
+  elif [[ "$input" == "-i" ]]; then
+    case_insenitive=true
+    other_args="$other_args $input"
   elif [[ "$input" =~ -.* ]]; then
     other_args="$other_args $input"
   else
     if [[ "${last_arg:0:1}" == "-" ]] && [[ "${last_arg:0:2}" != "--" ]] && [[ "efmdABCD" =~ .*"${last_arg: -1}".* ]]; then
       other_args="$other_args $input"
     else
+      input=`echo "$input" | sed 's/"/\\\"/'`
       other_search_terms=("${other_search_terms[@]}" "$input")
     fi
   fi
@@ -35,13 +40,22 @@ if [ $exclude_defaults = true ]; then
   grep_command="$grep_command --exclude-dir=.git --exclude-dir=node_modules --exclude=.tags --exclude=\"*.min.js\" --exclude=\"*.mo\" --exclude=jit-yc.js"
 fi
 
+
+if  $case_insenitive = true ]; then
+  grep_options="iPe"
+else
+  grep_options="Pe"
+fi
 for (( i=0; i<${#other_search_terms[@]}; i++ )); do
     search_term="${other_search_terms[$i]}"
     if [[ "$search_term" =~ "^".* ]];then
       search_term="(?<=[:-]\\x1b\\[m\\x1b\\[K)${search_term:1}"
     fi
-    grep_command="$grep_command | grep -Pe \"$search_term(?!(.*(\\x1b\\[[0-9;]*[mGKH])[-:](\\x1b\\[[0-9;]*[mGKH])+\d+(\\x1b\\[[0-9;]*[mGKH])+[-:]))\" --color=always"
+    if [[ ! "$search_term" =~ .*"$" ]];then
+      search_term="${search_term}(?!(.*(\\x1b\\[[0-9;]*[mGKH])[-:](\\x1b\\[[0-9;]*[mGKH])+\d+(\\x1b\\[[0-9;]*[mGKH])+[-:]))"
+    fi
+    grep_command="$grep_command | grep -${grep_options} \"${search_term}\" --color=always"
 done
 eval "$grep_command"
 
-#echo -e "\e[1;36m---------------------------Done---------------------------\e[0m"
+echo -e "\e[1;36m---------------------------Done---------------------------\e[0m"
