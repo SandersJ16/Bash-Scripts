22f87d5d1da1d6067fbd97d1b219178e8db737d3
Justin <justin@pointw.com>
2018-02-26 21:49:15 -0600

    Redid searchtext to allow multiple searches

--- searchtext.bash
+++ searchtext.bash
@@ -3,20 +3,42 @@
 exclude_defaults=true
 search_term=""
 other_args=""
+last_arg=""
+
+declare -a other_search_terms
+
 for input in "$@"; do
   if [[ ! "$input" =~ ^-.* ]] && [[ -z "$search_term" ]]; then
     search_term=`echo "$input" | sed 's/"/\\\"/'`
   elif [[ "$input" == "-v" ]]; then
     exclude_defaults=false
-  else
+  elif [[ "$input" =~ -.* ]]; then
     other_args="$other_args $input"
+  else
+    if [[ "${last_arg:0:2}" != "--" ]] && [[ "efmdABCD" =~ .*"${last_arg: -1}".* ]]; then
+      other_args="$other_args $input"
+    else
+      other_search_terms=("${other_search_terms[@]}" "$input")
+    fi
   fi
+  last_arg="$input"
 done
 
-grep_command="grep -nr . 2>/dev/null --color=always -Pe \"$search_term\" $other_args"
-if [ $exclude_defaults = true ]; then
- eval "$grep_command | grep -Eve '^[^:]*(\.git|\.tags|\.min\.js|\.mo|jit-yc\.js)'"
+
+if [ "${#other_search_terms[@]}" -eq 0 ]; then
+  color="auto"
 else
-  eval "$grep_command"
+  color="always"
 fi
-echo -e "\e[1;36m---------------------------Done---------------------------\e[0m"
+
+grep_command="grep -Inr . 2>/dev/null --color=$color -Pe \"$search_term\" $other_args"
+if [ $exclude_defaults = true ]; then
+  grep_command="$grep_command --exclude-dir=.git --exclude-dir=node_modules --exclude=.tags --exclude=\"*.min.js\" --exclude=\"*.mo\" --exclude=jit-yc.js"
+fi
+
+for (( i=0; i<${#other_search_terms[@]}; i++ )); do
+    grep_command="$grep_command | grep -Pe \"${other_search_terms[$i]}(?!(.*(\\x1b\\[[0-9;]*[mGKH])[-:](\\x1b\\[[0-9;]*[mGKH])+\d+(\\x1b\\[[0-9;]*[mGKH])+[-:]))\" --color=always"
+done
+eval "$grep_command"
+
+#echo -e "\e[1;36m---------------------------Done---------------------------\e[0m"
