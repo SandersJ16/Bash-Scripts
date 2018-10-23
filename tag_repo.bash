c202e67490f0cf87b27f06224b41e4d32b841bf9
Justin L Sanders <justin@pointw.com>
2018-10-23 01:29:47 -0500

    Update Git Ctag to force remove old ctag folder and update git message to match new git message

--- tag_repo.bash
+++ tag_repo.bash
@@ -5,9 +5,11 @@ while [ ! -d ".git" ] && [ `pwd` != "/" ]; do
 done
 path=`pwd`
 if [ "$path" == "/" ]; then
-    echo "fatal: Not in a git repository: .git"
+    echo "fatal: Not in a git repository (or any of the parent directories): .git"
     exit 1
 fi
+
 echo "Generating CTags in file $path/.tags"
+rm -r ".tags" 2>/dev/null
 ctags -R -o ".tags" * 2>/dev/null
 echo "Ctags Generated"
