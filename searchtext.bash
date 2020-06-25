7f3dbf506c666290ede86a05d67380b4d799dbcd
Justin L Sanders <justin@pointw.com>
2020-06-24 23:56:19 -0500

    Exclude more directories and files in searchtext by default

--- searchtext.bash
+++ searchtext.bash
@@ -58,8 +58,8 @@ fi
 grep_command="$base_grep_command -Inr . 2>/dev/null --color=$color ${grep_options} -e \"$search_term\" $other_args"
 if [ $exclude_defaults = true ]; then
   #list of directories and file types to exclude by default, will not be excluded if -V flag is used
-  declare -a exclude_dirs=(".git" "node_modules")
-  declare -a exclude_files=(".tags" "*.min.js" "*.mo" "*.po" "jit-yc.js" "*.min.css")
+  declare -a exclude_dirs=(".git" "node_modules" "vendor" "log")
+  declare -a exclude_files=(".tags" "*.min.js" "*.mo" "*.po" "jit-yc.js" "*.min.css" "*bundle.js")
 
   for exclude_dir in "${exclude_dirs[@]}"; do
     grep_command="$grep_command --exclude-dir=\"$exclude_dir\""
